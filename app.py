1eeb5b661c5a0674dc799380b89d08e7e8f6dd60
alcyone <alcyone@gmail.com>
2024-03-13 19:46:12 +0530

    'method'

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import psycopg2
 import jwt
 from flask import Flask, request, jsonify
-from datetime import datetime, timedelta 
-from functools import wraps  
+from datetime import datetime, timedelta
+from functools import wraps
 from flask_bcrypt import Bcrypt
 from flask_bcrypt import generate_password_hash
 from flask_bcrypt import check_password_hash
+from datetime import datetime
 
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
@@ -20,6 +21,7 @@ conn = psycopg2.connect(
 )
 
 cur = conn.cursor()
+
 
 def token_required(f):
     @wraps(f)
@@ -38,6 +40,7 @@ def token_required(f):
             return jsonify({'error': 'Invalid Token'}), 403
     return decorated
 
+
 def role_required(role_id):
     def decorator(func):
         @wraps(func)
@@ -49,12 +52,11 @@ def role_required(role_id):
                     payload = jwt.decode(
                         token, app.config['SECRET_KEY'], algorithms=["HS256"])
                     user_role = payload.get('role_id')
-                    cur.execute('''select * from users where role_id=%s''',(role_id,))
-                    data = cur.fetchall()
+                    
                     if user_role[0] == role_id:
-                        return jsonify(data)
-                    elif user_role[0]:
                         return func(*args, **kwargs)
+                    # elif user_role[0] == role_id:
+                    #     return jsonify(data)
                     else:
                         return jsonify({'error': 'insufficient permission'})
                 except jwt.ExpiredSignatureError:
@@ -70,6 +72,7 @@ def role_required(role_id):
 @app.route('/signup/user', methods=['POST'])
 def signup():
     data = request.json
+    uid = data.get("uid")
     uname = data.get("uname")
     email = data.get("email")
     password = data.get("password")
@@ -79,8 +82,8 @@ def signup():
     if existing_user:
         return jsonify({'message': 'User already exists'}), 400
     hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
-    cur.execute('''INSERT INTO users(uname,email,password,role_id) VALUES(%s,%s,%s,%s)''',
-                (uname, email, hashed_password, role_id))
+    cur.execute('''INSERT INTO users(uid,uname,email,password,role_id) VALUES(%s,%s,%s,%s,%s)''',
+                (uid, uname, email, hashed_password, role_id))
     conn.commit()
     return "Registerd successfully"
 
@@ -122,15 +125,45 @@ def logout():
     else:
         return jsonify({'error': 'token is missing'}), 403
 
-@app.route('/get_admin',methods=['GET'])
-@role_required(2) 
+
+@app.route('/get_admin', methods=['GET'])
+@role_required(2)
 def get_admin():
+    role_id=payload.get('role_id')
+    cur.execute('''select * from users where role_id=%s''', (role_id,))
+    data = cur.fetchall()
     return jsonify(data)
 
-@app.route('/get_users',methods=['GET'])
-@role_required(1) 
+
+@app.route('/get_users', methods=['GET'])
+@role_required(1)
 def get_users():
+    return 'users'
+
+
+@app.route('/create_post', methods=['POST'])
+@role_required(1)
+def create():
+    data = request.json
+    pid = data.get('id')
+    content = data.get('content')
+    likes = data.get('likes')
+    uid = data.get('uid')
+    id = data.get('id')
+    current_time = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+    cur.execute('''insert into post(pid,content,likes,created_at,uid,id) values(%s,%s,%s,%s,%s,%s)''',
+                (pid, content, likes, current_time, uid, id))
+    conn.commit()
     return jsonify(data)
+
+
+@app.route('/get_posts', methods=['GET'])
+@role_required(2)
+def get_posts():
+    cur.execute('''select pid,content,created_at from post''')
+    data = cur.fetchall()
+    return jsonify(data)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
