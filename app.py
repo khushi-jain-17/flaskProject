e18a0e3af1d1ede45d12bac9a45318d07cdaadb7
alcyone <alcyone@gmail.com>
2024-03-14 19:16:00 +0530

    'code'

--- app.py
+++ app.py
@@ -126,18 +126,68 @@ def logout():
 
 
 @app.route('/get_admin', methods=['GET'])
-@role_required(2)
+@token_required
 def get_admin():
-    role_id = payload.get('role_id')
-    cur.execute('''select * from users where role_id=%s''', (role_id,))
-    data = cur.fetchall()
-    return jsonify(data)
+    token = request.headers.get('Authorization')
+    if not token:
+        return jsonify({'error': 'token is missing'}), 403
+    try:
+        token = token.split(" ")[1]
+        payload = jwt.decode(
+            token, app.config['SECRET_KEY'], algorithms=['HS256'])
+        role_id = payload.get('role_id')
+        print(role_id[0])
+        if role_id[0] == 2:
+            cur.execute('''select * from users where role_id=%s''',
+                        (role_id[0],))
+            data = cur.fetchall()
+            conn.commit()
+            return jsonify(data)
+        else:
+            return jsonify({'error': 'insufficient permission'})
+    except jwt.ExpiredSignatureError:
+        return jsonify({'error': 'token has expired'}), 403
+    except jwt.InvalidTokenError:
+        return jsonify({'error': 'Invalid Token'}), 403
 
 
 @app.route('/get_users', methods=['GET'])
-@role_required(1)
+@token_required
 def get_users():
-    return 'users'
+    token = request.headers.get('Authorization')
+    if not token:
+        return jsonify({'error': 'token is missing'}), 403
+    try:
+        token = token.split(" ")[1]
+        payload = jwt.decode(
+            token, app.config['SECRET_KEY'], algorithms=['HS256'])
+        role_id = payload.get('role_id')
+        print(role_id[0])
+        if role_id[0] == 1:
+            cur.execute('''select * from users where role_id=%s''',
+                        (role_id[0],))
+            data = cur.fetchall()
+            conn.commit()
+            return jsonify(data)
+        else:
+            return jsonify({'error': 'insufficient permission'})
+    except jwt.ExpiredSignatureError:
+        return jsonify({'error': 'token has expired'}), 403
+    except jwt.InvalidTokenError:
+        return jsonify({'error': 'Invalid Token'}), 403
+
+
+@app.route('/connect', methods=['POST'])
+@role_required(1)
+def connect_users():
+    data = request.json
+    cid = data.get('cid')
+    follower_id = data.get('follower_id')
+    following_id = data.get('following_id')
+    cur.execute('''insert into connection(cid,follower_id,following_id) values(%s,%s,%s)''',
+                (cid, follower_id, following_id))
+    conn.commit()
+    return jsonify(data)
 
 
 @app.route('/create_post', methods=['POST'])
@@ -172,17 +222,102 @@ def get_post(id):
     conn.commit()
     return jsonify(data)
 
-@app.route('/update_post/<int:id>',methods=['PUT'])
+
+@app.route('/update_post/<int:id>', methods=['PUT'])
 @role_required(2)
 def update_post(id):
     pid = request.json['pid']
     content = request.json['content']
     cur.execute(
         '''UPDATE post SET pid=%s, content=%s WHERE id=%s''', (
-            pid,content, id,)
+            pid, content, id,)
     )
     conn.commit()
     return jsonify({'message': 'item updated successfully'})
+
+
+@app.route('/delete_post/<int:id>', methods=['DELETE'])
+@role_required(2)
+def delete_post(id):
+    cur.execute('''delete from post where id=%s''', (id,))
+    conn.commit()
+    return jsonify({'message': 'Post deleted successfully'})
+
+
+@app.route('/user/<int:uid>', methods=['GET'])
+@role_required(1)
+def posts_of_user(uid):
+    cur.execute(
+        '''select pid,content,created_at, uid from post where uid=%s''', (uid,))
+    data = cur.fetchall()
+    conn.commit()
+    return jsonify(data)
+
+
+@app.route('/like_post', methods=['POST'])
+@role_required(1)
+def like_post():
+    data = request.json
+    uid = data.get('uid')
+    likes = data.get('likes')
+    pid = data.get('pid')
+    cur.execute(
+        '''insert into post(uid,likes,pid) values(%s,%s,%s)''', (uid, likes, pid))
+    conn.commit()
+    return jsonify(data)
+
+
+@app.route('/likes_count/<int:pid>', methods=['GET'])
+@role_required(1)
+def count_likes(pid):
+    cur.execute(
+        '''select count(uid) from post where pid=%s and likes='True' ''', (pid,))
+    data = cur.fetchall()
+    return jsonify(data)
+
+
+@app.route('/posts_liked_by_user/<int:uid>', methods=['GET'])
+@role_required(1)
+def posts_liked_by_user(uid):
+    cur.execute('''select pid,content,likes from post where uid=%s''', (uid,))
+    data = cur.fetchall()
+    return jsonify(data)
+
+
+@app.route('/liked_by/<int:pid>', methods=['GET'])
+@role_required(1)
+def people_liked_the_post(pid):
+    cur.execute('''select users.uname, users.uid, post.content from users inner join post on users.uid=post.uid where post.pid=%s and likes='True' ''', (pid,))
+    data = cur.fetchall()
+    return jsonify(data)
+
+
+@app.route('/home/<int:uid>', methods=['GET'])
+@role_required(1)
+def home(uid):
+    cur.execute('''select pid from post where uid=%s and likes='True' ''', (uid,))
+    p = cur.fetchone()
+    cur.execute(
+        '''select likedata.name,likedata.content, likedata.tc from likedata inner join post on likedata.uid = post.uid where post.pid=%s  ''',(p,))
+    data = cur.fetchall()
+    return jsonify(data)
+
+
+@app.route('/create/likedata', methods=['POST'])
+def create_likedata():
+    data = request.json
+    lid = data.get('lid')
+    post_id = data.get('post_id')
+    name = data.get('name')
+    content = data.get('content')
+    cur.execute(
+        '''select count(uid) from post where pid=%s and likes='True' ''',(post_id,))
+    total = cur.fetchone()
+    cur.execute('''insert into likedata(lid,post_id,name,content,tc) values(%s,%s,%s,%s,%s)''',
+                (lid, post_id, name, content, total))
+    conn.commit()
+    return jsonify(data)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
